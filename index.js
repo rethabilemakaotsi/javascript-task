172acc6cf2d7508e71e1f0b5f9910cfd1b2e3018
rethabile <rethabilemakaotsi17@gmail.com>
2023-11-20 11:12:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let first_number = 15;
+let second_number = 17;
+
+let temp = first_number;
+first_number = second_number;
+second_number = temp;
+
+console.log("After swapping: first_number =", first_number, ", second_number =", second_number);
+
+let first_number = 15;
+let second_number = 17;
+
+first_number = first_number + second_number;
+second_number = first_number - second_number;
+first_number = first_number - second_number;
+
+console.log("After swapping: first_number =", first_number, ", second_number =", second_number);
+
+let first_number = 15;
+let second_number = 17;
+
+first_number = first_number + second_number;
+second_number = first_number - second_number;
+first_number = first_number - second_number;
+
+console.log("After swapping: first_number =", first_number, ", second_number =", second_number);
